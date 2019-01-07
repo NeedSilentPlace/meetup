a700681726fc96a60e95b48b9183b97072963ca4
hank <hank@gmail.com>
2019-01-08 03:32:55 +0900

    Add mapStyle file and modify index.js
    mapStyle.js is made for code-readability.
    index.js is modified for code-consistency and code-readability.

--- app/index.js
+++ app/index.js
@@ -1,122 +1,46 @@
 // Load application styles
 import 'styles/index.less';
-
+import {mapStyle} from 'mapStyle.js';
 // ================================
 // START YOUR APP HERE
 // ================================
 const favoriteStorage = window.localStorage;
-document.querySelector('.favoriteNumber').textContent = favoriteStorage.length - 1;
-
 const home = document.querySelector('.home');
-home.addEventListener('click', function() {
-  window.location.reload();
-});
-
-$.ajax({
-  url: "https://maps.googleapis.com/maps/api/js?key=AIzaSyAeOFU6LK6dWgzZHUFdeCIKiUYdu5BzPGU&language=en",
-  dataType: 'jsonp',
-  success: initMap
-});
-
 const sweden = {lat: 59.32, lng: 18.07};
 const markersArray = [];
+
+const meetupLogo = document.querySelector('.meetupLogo');
+const addButton = document.querySelector('.add');
+const deleteButton = document.querySelector('.delete');
+const heartIcon = document.querySelector('.favorite');
+const exit = document.querySelector('.exit');
+const selectedData = {};
+let isHeartClicked = false;
 let map;
 
-const icon = document.querySelector('.icon');
+document.querySelector('.favoriteNumber').textContent = favoriteStorage.length - 1;
+addButton.addEventListener('click', addFavorite);
+deleteButton.addEventListener('click', deleteFavorite);
+heartIcon.addEventListener('click', favoriteVisualizer);
+exit.addEventListener('click', exitFavorite);
+home.addEventListener('click', () => {
+  window.location.reload();
+});
 
 function initMap() {
     map = new google.maps.Map(document.getElementById('map'), {
     center: sweden,
     zoom: 10,
-    styles: [
-      {elementType: 'geometry', stylers: [{color: '#242f3e'}]},
-      {elementType: 'labels.text.stroke', stylers: [{color: '#242f3e'}]},
-      {elementType: 'labels.text.fill', stylers: [{color: '#746855'}]},
-      {
-        featureType: 'administrative.locality',
-        elementType: 'labels.text.fill',
-        stylers: [{color: '#d59563'}]
-      },
-      {
-        featureType: 'poi',
-        elementType: 'labels.text.fill',
-        stylers: [{color: '#d59563'}]
-      },
-      {
-        featureType: 'poi.park',
-        elementType: 'geometry',
-        stylers: [{color: '#263c3f'}]
-      },
-      {
-        featureType: 'poi.park',
-        elementType: 'labels.text.fill',
-        stylers: [{color: '#6b9a76'}]
-      },
-      {
-        featureType: 'road',
-        elementType: 'geometry',
-        stylers: [{color: '#38414e'}]
-      },
-      {
-        featureType: 'road',
-        elementType: 'geometry.stroke',
-        stylers: [{color: '#212a37'}]
-      },
-      {
-        featureType: 'road',
-        elementType: 'labels.text.fill',
-        stylers: [{color: '#9ca5b3'}]
-      },
-      {
-        featureType: 'road.highway',
-        elementType: 'geometry',
-        stylers: [{color: '#746855'}]
-      },
-      {
-        featureType: 'road.highway',
-        elementType: 'geometry.stroke',
-        stylers: [{color: '#1f2835'}]
-      },
-      {
-        featureType: 'road.highway',
-        elementType: 'labels.text.fill',
-        stylers: [{color: '#f3d19c'}]
-      },
-      {
-        featureType: 'transit',
-        elementType: 'geometry',
-        stylers: [{color: '#2f3948'}]
-      },
-      {
-        featureType: 'transit.station',
-        elementType: 'labels.text.fill',
-        stylers: [{color: '#d59563'}]
-      },
-      {
-        featureType: 'water',
-        elementType: 'geometry',
-        stylers: [{color: '#17263c'}]
-      },
-      {
-        featureType: 'water',
-        elementType: 'labels.text.fill',
-        stylers: [{color: '#515c6d'}]
-      },
-      {
-        featureType: 'water',
-        elementType: 'labels.text.stroke',
-        stylers: [{color: '#17263c'}]
-      }
-    ]
+    styles: mapStyle
   });
 
   map.addListener('click', function(ev) {
-    let informationBox = document.querySelector('.informationBox');
-    let meetupBox = document.querySelector('.meetupBox');
-    let newMeetupBox = document.createElement('div');
+    const informationBox = document.querySelector('.informationBox');
+    const meetupBox = document.querySelector('.meetupBox');
+    const newMeetupBox = document.createElement('div');
 
-    icon.classList.remove('displayNone');
-    icon.classList.add('loading');
+    meetupLogo.classList.remove('displayNone');
+    meetupLogo.classList.add('loading');
     informationBox.removeChild(meetupBox);
     newMeetupBox.classList.add('meetupBox');
     informationBox.appendChild(newMeetupBox);
@@ -143,7 +67,7 @@ function deleteMarker() {
 }
 
 function requestMeetupData(lat, lng) {
-  let url = `https://api.meetup.com/find/upcoming_events?&sign=true&key=603c3d73f4e35466617592a56d31&page=10&radius=8&lon=${lng}&lat=${lat}`;
+  const url = `https://api.meetup.com/find/upcoming_events?&sign=true&key=603c3d73f4e35466617592a56d31&page=10&radius=8&lon=${lng}&lat=${lat}`;
 
   $.ajax({
     url: url,
@@ -156,13 +80,13 @@ function meetupDataFilter(area) {
   const eventData = [];
 
   if(area.data.errors) {
-    icon.classList.remove('loading');
+    meetupLogo.classList.remove('loading');
     return alert('There is no city.. Please click again!');
   } else {
     const events = area.data.events;
 
     if(!events.length) {
-      icon.classList.remove('loading');
+      meetupLogo.classList.remove('loading');
       return alert('There is no meetup.. Please click another city!');
     }
     
@@ -194,7 +118,6 @@ function meetupDataFilter(area) {
 
     getHostData(eventData);
   }
-
 }
 
 function getHostData(eventData) {
@@ -202,7 +125,7 @@ function getHostData(eventData) {
   const hostPromiseArray = [];
 
   for(let i = 0; i < eventData.length; i++) {
-    let hostData = new Promise(function(resolve, reject) {
+    const hostData = new Promise(function(resolve, reject) {
       $.ajax({
         url: `https://api.meetup.com/${eventData[i].urlName}/events/${eventData[i].id}/hosts`,
         dataType: 'jsonp',
@@ -214,7 +137,7 @@ function getHostData(eventData) {
 
   Promise.all(hostPromiseArray).then((host) => {
     for(let i = 0; i < host.length; i++) {
-      let nameAndPhoto = host[i].data;
+      const nameAndPhoto = host[i].data;
       const name = [];
       const photo = [];
 
@@ -234,71 +157,69 @@ function getHostData(eventData) {
     for(let k = 0; k < finalData.length; k++) {
       document.querySelector('.meetupBox').appendChild(dataVisualizer(finalData[k]));
     }
+
+    meetupLogo.classList.add('displayNone');
+    meetupLogo.classList.remove('loading');
     document.querySelector('.meetupBox').classList.add('showBlinder');
-    let icon = document.querySelector('.icon');
-    icon.classList.add('displayNone');
-    icon.classList.remove('loading');
   });
 }
 
-const selectedData = {};
-
 function dataVisualizer(data, isFavorite = false) {
-  let box = document.createElement('div');
-  box.classList.add('unit');
-  if(isFavorite) {
-    box.classList.add('favoriteColor');
-  }
-  let clickCircle = document.createElement('div');
-  clickCircle.classList.add('clickCircle');
+  const unit = document.createElement('div');
+  const contentBox = document.createElement('div');  
+  const nameBox = document.createElement('div');
+  const numberBox = document.createElement('div');
+  const pictureBox = document.createElement('div');
+  const eventName = document.createElement('a');
+  const groupName = document.createElement('span');
+  const time = document.createElement('span');
+  const rsvp = document.createElement('span');
+  const br = document.createElement('br');
+  const clickCircle = document.createElement('div');
 
+
+  unit.classList.add('unit');
+  clickCircle.classList.add('clickCircle');
+  if(isFavorite) {
+    unit.classList.add('favoriteColor');
+  }
   if(!isFavorite) {
     clickCircle.textContent = '+';
   } else {
     clickCircle.textContent = '-';
   }
 
-  let contentBox = document.createElement('div');
   contentBox.classList.add('contentBox');
-  let nameBox = document.createElement('div');
-  let numberBox = document.createElement('div');
-  let pictureBox = document.createElement('div');
-  let eventName = document.createElement('a');
   eventName.href = data.eventLink;
   eventName.target = '_blank';
   eventName.classList.add('eventName');
   eventName.textContent = data.eventName;
-  let groupName = document.createElement('span');
-  let br = document.createElement('br');
   groupName.classList.add('groupName');
   groupName.textContent = `by ${data.groupName}`;
-  nameBox.appendChild(eventName);
-  nameBox.appendChild(br);
-  nameBox.appendChild(groupName);
-  let time = document.createElement('span');
   time.classList.add('time');
   time.textContent = data.time;
-  let rsvp = document.createElement('span');
   rsvp.classList.add('rsvp');
   rsvp.textContent = `rsvp limit : ${data.rsvpLimit} | rsvp :  ${data.yesRsvp} | wait list : ${data.waitlist}`;
-  numberBox.appendChild(time);
-  numberBox.appendChild(rsvp);
-
 
   for(let i = 0; i < data.name.length; i++) {
-    let img = document.createElement('img');
+    const img = document.createElement('img');
+
     img.src = data.photo[i];
     img.title = data.name[i];
     img.classList.add('picture');
     pictureBox.appendChild(img);
   }
 
+  nameBox.appendChild(eventName);
+  nameBox.appendChild(br);
+  nameBox.appendChild(groupName);
+  numberBox.appendChild(time);
+  numberBox.appendChild(rsvp);
   contentBox.appendChild(nameBox);
   contentBox.appendChild(numberBox);
   contentBox.appendChild(pictureBox);
-
-  box.appendChild(clickCircle);
-  box.appendChild(contentBox);
+  unit.appendChild(clickCircle);
+  unit.appendChild(contentBox);
 
   if(!isFavorite) {
     clickCircle.addEventListener('click', (ev) => {
@@ -322,82 +243,64 @@ function dataVisualizer(data, isFavorite = false) {
     });
   }
 
-  return box;
+  return unit;
 }
 
-const addButton = document.querySelector('.add');
+function addFavorite() {
+  const selectedEvent = document.querySelectorAll('.selected');
+  const meetupBox = document.querySelector('.meetupBox'); 
 
-addButton.addEventListener('click', function(ev) {
+  heartIcon.classList.add('heartSizeUp');
+
   for(let key in selectedData) {
     favoriteStorage.setItem(JSON.stringify(key), selectedData[key]);
     delete selectedData[key];
   }
   
-  let selectedEvent = document.querySelectorAll('.selected');
-  
   for(let i = 0; i < selectedEvent.length; i++) {
       selectedEvent[i].parentElement.classList.add('addAnimation');
   }
 
-  let heartIcon = document.querySelector('.favorite');
-  heartIcon.classList.add('heartSizeUp');
-
   setTimeout(function() {
-    let meetupBox = document.querySelector('.meetupBox');
     for(let j = 0; j < selectedEvent.length; j++) {
       meetupBox.removeChild(selectedEvent[j].parentElement);
     }
     heartIcon.classList.remove('heartSizeUp');
     document.querySelector('.favoriteNumber').textContent = favoriteStorage.length - 1;
   }, 500);
-});
+}
 
-const deleteButton = document.querySelector('.delete');
+function deleteFavorite() {
+  const selectedEvent = document.querySelectorAll('.selected');
+  const favoriteBox = document.querySelector('.favoriteBox');
 
-deleteButton.addEventListener('click', function(ev) {
+  heartIcon.classList.add('heartSizeDown');
+
   for(let key in selectedData) {
     favoriteStorage.removeItem(JSON.stringify(key));
     delete selectedData[key];
   }
 
-  let selectedEvent = document.querySelectorAll('.selected');
-
   for(let i = 0; i < selectedEvent.length; i++) {
     selectedEvent[i].parentElement.classList.add('addAnimation');
   }
 
-  let heartIcon = document.querySelector('.favorite');
-  heartIcon.classList.add('heartSizeDown');
-
   setTimeout(function() {
-    let favoriteBox = document.querySelector('.favoriteBox');
     for(let j = 0; j < selectedEvent.length; j++) {
       favoriteBox.removeChild(selectedEvent[j].parentElement);
     }
     document.querySelector('.favoriteNumber').textContent = favoriteStorage.length - 1;
     heartIcon.classList.remove('heartSizeDown');
   }, 500);
-});
+}
 
-
-let isFavoriteClicked = false;
-const favorite = document.querySelector('.favorite');
-favorite.addEventListener('click', favoriteVisualizer);
-
-function favoriteVisualizer(ev) {
-  if(isFavoriteClicked) {
+function favoriteVisualizer() {
+  if(isHeartClicked) {
     return;
   }
-  isFavoriteClicked = true;
   const favoriteBox = document.querySelector('.favoriteBox');
-  
-  for(let key in favoriteStorage) {
-    if(favoriteStorage.hasOwnProperty(key) && key !== 'loglevel:webpack-dev-server') {
-      let favoriteData = JSON.parse(favoriteStorage.getItem(key));
-      favoriteBox.appendChild(dataVisualizer(favoriteData, true));
-    }
-  }
 
+  isHeartClicked = true;
   document.querySelector('.blinder').classList.add('showBlinder');
   document.querySelector('.mapBox').classList.add('disappear');
   document.querySelector('.meetupBox').classList.add('disappear');
@@ -406,15 +309,19 @@ function favoriteVisualizer(ev) {
   document.querySelector('.favorite').classList.add('moveHeart');
   document.querySelector('.add').classList.add('displayNone');
   favoriteBox.classList.add('showFavorite');
+
+  for(let key in favoriteStorage) {
+    if(favoriteStorage.hasOwnProperty(key) && key !== 'loglevel:webpack-dev-server') {
+      let favoriteData = JSON.parse(favoriteStorage.getItem(key));
+      favoriteBox.appendChild(dataVisualizer(favoriteData, true));
+    }
+  }
 }
 
-const exit = document.querySelector('.exit');
-exit.addEventListener('click', exitFavorite);
-
-function exitFavorite(ev) {
+function exitFavorite() {
   const favoriteBox = document.querySelector('.favoriteBox');
   
-  isFavoriteClicked = false;
+  isHeartClicked = false;
   document.querySelector('.blinder').classList.remove('showBlinder');
   document.querySelector('.mapBox').classList.remove('disappear');
   document.querySelector('.meetupBox').classList.remove('disappear');
@@ -428,6 +335,12 @@ function exitFavorite(ev) {
     favoriteBox.removeChild(favoriteBox.children[0]);
   }
 }
+
+$.ajax({
+  url: "https://maps.googleapis.com/maps/api/js?key=AIzaSyAeOFU6LK6dWgzZHUFdeCIKiUYdu5BzPGU&language=en",
+  dataType: 'jsonp',
+  success: initMap
+});
 
 // You can use jquery for ajax request purpose only.
 import $ from 'jquery';
